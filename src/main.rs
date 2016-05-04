da43369555b4261e527bb7ddc6ba65be05960821
Michael Aaron Murphy <mmstickman@gmail.com>
2016-05-04 11:45:16 -0400

    A Handful of Fixes

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,25 @@ fn main() {
     }
 }
 
+fn match_language(input: &str) -> String {
+    match input {
+        "Chinese"   => "ZH-CN".to_string(),
+        "English"   => "EN".to_string(),
+        "Esperanto" => "EO".to_string(),
+        "French"    => "FR".to_string(),
+        "German"    => "DE".to_string(),
+        "Italian"   => "IT".to_string(),
+        "Japanese"  => "JA".to_string(),
+        "Korean"    => "KO".to_string(),
+        "Russian"   => "RU".to_string(),
+        "Spanish"   => "ES".to_string(),
+        _ => {
+            println!("Language Not Supported");
+            std::process::exit(1);
+        }
+    }
+}
+
 /// Launch the GTK GUI
 fn launch_gui() {
     // Initialize GTK
@@ -82,14 +101,11 @@ fn launch_gui() {
     let window: Window = builder.get_object("main_window").unwrap();
     let translate_button: Button = builder.get_object("translate_button").unwrap();
     let translation_input: TextView = builder.get_object("translation_input").unwrap();
-    let translation_output: TextView = builder.get_object("translation_output").unwrap();
     let language_box: ComboBoxText = builder.get_object("language").unwrap();
 
     // Add a TextBuffer to every TextView
     let input_buffer = TextBuffer::new(Some(&TextTagTable::new()));
     translation_input.set_buffer(Some(&input_buffer));
-    let output_buffer = TextBuffer::new(Some(&TextTagTable::new()));
-    translation_output.set_buffer(Some(&output_buffer));
 
     // Wrap translation_button so that it may be borrowed multiple times
     let wrapped_translation_button = Rc::new(RefCell::new(translate_button));
@@ -102,14 +118,14 @@ fn launch_gui() {
             let string = buffer.get_text(&buffer.get_start_iter(), &buffer.get_end_iter(), false).unwrap();
 
             // Get the langauge combo box's text.
-            let language = language_box.get_active_text().unwrap();
+            let language = match_language(language_box.get_active_text().unwrap().as_str());
 
             // Translate the text.
             let mut translation = String::new();
             translate(&string, language.as_str(), &mut translation);
 
-            // Update the translation output text view.
-            translation_output.get_buffer().unwrap().set_text(translation.as_str());
+            // Immediately translate the text
+            translation_input.get_buffer().unwrap().set_text(translation.as_str());
         });
     }
 
@@ -154,14 +170,13 @@ fn translate(input: &str, language: &str, output: &mut String) {
 
 /// Take the raw response from Google and parse the translation only.
 fn parse_message(input: &str, translation: &mut String) {
-    let mut iterator    = input.chars().skip(4);
     let mut escape      = false;
     let mut ignore      = false;
     let mut found_match = false;
-    let mut matched: i8 = 0;
+    let mut matched: u8 = 0;
 
     // Loop until ',,,0]]' is found
-    while let Some(character) = iterator.next() {
+    for character in input.chars().skip(4) {
         if found_match {
             matched = match matched {
                 0 => 1,
@@ -179,17 +194,15 @@ fn parse_message(input: &str, translation: &mut String) {
                 (5, _)   => {ignore = false; found_match = true; 0 }
                 _ => 0
             };
+        } else if character == '\\' && !escape {
+            escape = true;
+        } else if escape {
+            translation.push(character);
+            escape = false;
+        } else if character == '"' {
+            ignore = true;
         } else {
-            if character == '\\' && !escape {
-                escape = true;
-            } else if escape {
-                translation.push(character);
-                escape = false;
-            } else if character == '"' {
-                ignore = true;
-            } else {
-                translation.push(character);
-            }
+            translation.push(character);
         }
     }
 }
